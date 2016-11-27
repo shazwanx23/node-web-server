ba4f03393203fe1df545b257f90cbf9ce9f66b6e
shazwan <shazwanx23@gmail.com>
2016-11-27 20:44:28 +0800

    middleware and maintenance mode

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n');
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs', {
+// 		pageTitle: 'Maintenance Page'
+// 	});
+// });
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
